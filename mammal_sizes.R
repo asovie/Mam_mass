bbe758c39bfea5cdaed406c4b0507cb5f5228128
Adia Sovie <asovie@ufl.edu>
2015-11-13 08:53:23 -0500

    remove historic ext. calculate mean log_mass by status

--- mammal_sizes.R
+++ mammal_sizes.R
@@ -2,3 +2,5 @@ mammal_sizes<-read.table("http://www.esapubs.org/archive/ecol/E084/094/MOMv3.3.t
 colnames(mammal_sizes) <- c("continent", "status", "order", 
                             "family", "genus", "species", "log_mass", "combined_mass", 
                             "reference")
+mammal_sizes_red<-subset(mammal_sizes, !mammal_sizes$status=="historical", drop=TRUE)
+mean_mass<-tapply(mammal_sizes_red$log_mass, mammal_sizes_red$status, mean, na.rm=TRUE)
