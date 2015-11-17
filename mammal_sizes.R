e259058089ce02c2a4658b84f3a9ff0dc9f330d8
Adia Sovie <asovie@ufl.edu>
2015-11-16 22:27:29 -0500

    plot hist of log_mass by continent and status

--- mammal_sizes.R
+++ mammal_sizes.R
@@ -1,6 +1,9 @@
 #Mammal body size and extinction
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+library(grDevices)
+
 mammal_sizes<-read.table("http://www.esapubs.org/archive/ecol/E084/094/MOMv3.3.txt", header=F, sep="\t",stringsAsFactors = FALSE, na.strings = "-999")
 
 colnames(mammal_sizes) <- c("continent", "status", "order", 
@@ -12,9 +15,12 @@ mammal_sizes_red<-subset(mammal_sizes_red, !mammal_sizes_red$status=="introducti
 
 mean_mass<-tapply(mammal_sizes_red$combined_mass, mammal_sizes_red$status, mean, na.rm=TRUE) #calculate mean combined_mass by status
 
-mean_mass_con<-aggregate(combined_mass ~ status + continent, data = mammal_sizes_red, FUN = mean)#calculate mean log_mass by status and con
+mean_mass_con<-aggregate(combined_mass ~ status + continent, data = mammal_sizes_red, FUN = mean)#calculate mean combined_mass by status and con
 
-write.table(spread(mean_mass_con, status,log_mass), "continent_mass_differences.csv", sep=",")
+write.table(spread(mean_mass_con, status,combined_mass), "continent_mass_differences.csv", sep=",")
 
+sp<-qplot(log_mass, data=mammal_sizes_red, geom="histogram") #graph distribution of log_mass of extinct and extant species
+log_mass_plot<-sp + facet_grid(continent ~ status) 
 
-
+png("log_mass_plot.png")
+dev.off()
